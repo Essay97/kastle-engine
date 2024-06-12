6a4691bb95fa07d97fea556b56a393258de6c673
Saggiorato Enrico <saggiorato.enrico@gmail.com>
2024-06-13 01:43:21 +0200

    delete test command

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,5 +4,5 @@ import com.github.ajalt.clikt.core.subcommands
 import io.github.essay97.kastle.cli.commands.*
 
 fun main(args: Array<String>) = Kastle()
-    .subcommands(List(), Install(), Uninstall(), Play(), Info(), Test())
+    .subcommands(List(), Install(), Uninstall(), Play(), Info())
     .main(args)
